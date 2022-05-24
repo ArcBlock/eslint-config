b30bb7c4a30fb7053cc6caa5a4ee2ad8e7ec817c
zhanghan <Lancelot_Lewis@163.com>
2022-05-25 00:32:05 +0800

    chore: update release scripts

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -1,16 +1,8 @@
-// 用于发布 github release
-
-import { $, fs, path } from 'zx';
-import { execSync } from 'child_process';
-
-execSync(`zx ${path.join(__dirname, 'bump-version.mjs')} --quiet`, { stdio: 'inherit' });
-
+import { $, fs } from 'zx';
 const { version } = await fs.readJSON('package.json');
 
 await $`git add .`;
-// await $`git commit -m "chore: release v${version}"`;
-// await $`git tag v${version}`;
-// await $`git push`;
-// await $`git push origin --tags`;
-// await $`blocklet version ${version}`;
-// await $`git add CHANGELOG.md blocklet.yml`;
+await $`git commit -m "chore: release v${version}"`;
+await $`git tag v${version}`;
+await $`git push`;
+await $`git push origin --tags`;
