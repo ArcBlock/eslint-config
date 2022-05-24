cca3f4281af8777d9e8e5add87cb472b5e2f4fde
zhanghan <Lancelot_Lewis@163.com>
2022-05-24 23:46:07 +0800

    feat: add example for eslint-config

---
+++ examples/react/src/example.jsx
@@ -0,0 +1,7 @@
+export default function Test() {
+  const msg = window?.a?.b;
+
+  console.error(msg);
+
+  return <div> hi </div>;
+}
---
+++ examples/react/test/abc.test.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line import/prefer-default-export
+export function myHelper() {}
+
+module.exports = function test() {};
+
+module.exports = {
+  something: 'that should be moved to a non-test file',
+};
+
+const myBeverage = {
+  delicious: true,
+  sour: false,
+  one: null,
+};
+
+describe('my beverage', () => {
+  test('is delicious', () => {
+    expect(myBeverage.delicious).toBeTruthy();
+  });
+
+  test('is not sour', () => {
+    expect(myBeverage.sour).toBeFalsy();
+  });
+  test('is null', () => {
+    expect(myBeverage.one).toBe(null);
+  });
+});
